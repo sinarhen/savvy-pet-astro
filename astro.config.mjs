62a0bef822b218f2ae52ab4c4d1dbf799e09dcd4
sinarhen <nicklchannel8@gmail.com>
2025-08-03 01:57:59 +0300

    proj init and navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,18 +6,17 @@ import tailwindcss from '@tailwindcss/vite';
 import sanity from '@sanity/astro';
 import react from '@astrojs/react';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [
-    sanity({
-      projectId: "your-project-id",
-      dataset: "production",
-      useCdn: false,
-    }),
-    react()
-  ]
+  integrations: [sanity({
+    projectId: "your-project-id",
+    dataset: "production",
+    useCdn: false,
+  }), react(), icon()]
 });
